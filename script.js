4cce8cab01555b4363b2906c6d8d886c8528b95e
Mullins69 <91262437+Mullins69@users.noreply.github.com>
2021-11-29 14:48:20 +0200

    BAM!

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function celctofah(){
+    let fah = document.getElementById("input1").value;;
+    let converted = (Number(fah) * 1.8) + 32;
+    if(fah == Number(fah)){
+        document.getElementById("Output1").innerHTML = converted + "°F";
+    }
+    else{
+        document.getElementById("Output1").innerHTML = "Enter Numbers only";
+    }
+    
+}
+
+function fahtocelc(){
+    let celc = document.getElementById("input2").value;
+    let converted = (Number(celc) - 32) * 5/9;
+    if(celc == Number(celc)){
+        document.getElementById("Output2").innerHTML = converted.toFixed(2) + "°C";
+    }
+    else{
+        document.getElementById("Output2").innerHTML = "Enter Numbers only";
+    }
+}
+
