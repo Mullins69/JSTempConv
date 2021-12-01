e789cc5034b0e5f1d9a10deb3d7e8deed246187c
Mullins69 <91262437+Mullins69@users.noreply.github.com>
2021-12-01 12:05:45 +0200

    added toFixed(0)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function celctofah(){
     let fah = document.getElementById("input1").value;;
     let converted = (Number(fah) * 1.8) + 32;
     if(fah == Number(fah)){
-        document.getElementById("Output1").innerHTML = converted + "°F";
+        document.getElementById("Output1").innerHTML = converted.toFixed(0) + "°F";
     }
     else{
         document.getElementById("Output1").innerHTML = "Enter Numbers only";
@@ -15,7 +15,7 @@ function fahtocelc(){
     let celc = document.getElementById("input2").value;
     let converted = (Number(celc) - 32) * 5/9;
     if(celc == Number(celc)){
-        document.getElementById("Output2").innerHTML = converted.toFixed(2) + "°C";
+        document.getElementById("Output2").innerHTML = converted.toFixed(0) + "°C";
     }
     else{
         document.getElementById("Output2").innerHTML = "Enter Numbers only";
